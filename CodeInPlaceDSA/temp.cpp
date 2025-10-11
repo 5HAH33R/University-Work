11db86ab85f580d01f96b062b9b2b518953f6490
5HAH33R <shaheer.khan2@gmail.com>
2025-10-11 15:18:33 +0500

    temp

--- CodeInPlaceDSA/temp.cpp
+++ CodeInPlaceDSA/temp.cpp
@@ -1,25 +1,95 @@
-#include <iostream>
-#include <set>
-#include <vector>
+#include<iostream>
+#include<set>
+#include<vector>
+#include<stack>
+#include<sstream>
 
 using namespace std;
 
-void printArr(vector<int> &arr){
-    for(auto i : arr){
-        cout << i << " ";
+bool isPrime(int n){
+    int div = 0;
+    if(n <= 1){
+        return false;
     }
-    cout << endl;
-
-
-
+    for(int i = 0 ; i < n ; i++){
+        if(n % i == 0 ){
+            div++;
+        }
+        if(div > 2){
+            return false;
+        }
+    }
+    return true;
 }
+
+int nextPrime(int n){
+    int ans = n+1;
+    while(isPrime(ans) != true){
+        ans = ans +1;
+    }
+    return ans;
+    
+}
+
+vector<int> waiter(vector<int> numbers , int q){
+    int count = 1;
+    int prime = 2;
+    vector<int> answer;
+    vector<int> A;
+    vector<int> B;
+
+    while(count != q){
+        if(count == 1){
+            for(auto i : numbers){
+                if(prime % i == 0){
+                    B.push_back(i);
+                }
+                else{
+                    A.push_back(i);
+                }
+
+            }
+        }
+        else{
+            for(auto i : A){
+                if(prime % i == 0){
+                    B.push_back(i);
+                    A.pop_back();
+                }
+
+            }
+        
+        }
+        count++;
+        for(auto i : B){
+            answer.push_back(i);
+            B.pop_back();
+        }
+    }
+    for(auto i : A){
+        answer.push_back(i);
+    }
+    return answer;
+}
+
+
 int main(){
-    set<vector<int>> ans;
-    ans.insert({1,2,2});
-    ans.insert({2,1,2});
-    for(auto i : ans){
-        printArr(i);
+    string line;
+    int n = 0 , q = 0;
+    int num;
+    vector<int> numbers;
+    getline(cin, line);
+    stringstream ss1(line);
+    ss1 >> n >> q;
+    getline(cin , line);
+    stringstream ss(line);
+    while(ss >> num){
+        numbers.push_back(num);
     }
 
-  return 0;
+    vector<int> answer = waiter(numbers,q);
+    for(auto i : answer){
+        cout << i <<" ";
+    }
+
 }
